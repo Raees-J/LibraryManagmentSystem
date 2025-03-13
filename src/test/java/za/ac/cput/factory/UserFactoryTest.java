d66513859d9e97552530543fc5732ee73e5fde4d
raees johaadien <163304344+Raees-J@users.noreply.github.com>
2025-03-13 15:31:44 +0200

    User, userFactory and user test with main

---
+++ src/test/java/za/ac/cput/factory/UserFactoryTest.java
@@ -0,0 +1,35 @@
+package za.ac.cput.factory;
+import org.junit.jupiter.api.*;
+import za.ac.cput.domain.User;
+import static org.junit.jupiter.api.Assertions.*;
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class UserFactoryTest {
+    private static User u1 = userFactory.createUser("230558135","Raees","58 Bellmore Avenue");
+
+    private static User u2 = userFactory.createUser("220447149", "boeta", "123 goege drive", "0836379078");
+
+    private static User u3 = userFactory.createUser("217693950", "","","");
+
+    @Test
+    @Order(1)
+    public void testCreateUser() {
+        assertNotNull(u1);
+        System.out.println(u1.toString());
+    }
+
+    @Test
+    @Order(2)
+    public void testCreateUserWithAllAttributes() {
+        assertNotNull(u2);
+        System.out.println(u2.toString());
+    }
+
+    @Test
+    @Order(3)
+    public void testCreateUserThatFails() {
+        assertNotNull(u3);
+        System.out.println(u3.toString());
+    }
+
+
+}
