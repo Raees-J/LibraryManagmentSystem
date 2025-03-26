2b9ca9500c1dce1cb0221a8e833f97197b011cad
raees johaadien <163304344+Raees-J@users.noreply.github.com>
2025-03-26 13:32:27 +0200

    Update UserFactoryTest.java

--- src/test/java/za/ac/cput/factory/UserFactoryTest.java
+++ src/test/java/za/ac/cput/factory/UserFactoryTest.java
@@ -1,3 +1,9 @@
+/*
+UserFactoryTest.java
+User model class
+Author: Raees Johaadien (230558135)
+Date: 14 March 2025
+*/
 package za.ac.cput.factory;
 import org.junit.jupiter.api.*;
 import za.ac.cput.domain.User;
